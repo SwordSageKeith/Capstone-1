e2837a1e025a85384553c3095ffc684ba788621f
Keith Prince <keithcprince@gmail.com>
2019-09-20 15:40:42 -0400

    done

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: 'https://character-manager.herokuapp.com/',
+  API_ENDPOINT: 'https://character-manager-api.herokuapp.com/',
   TOKEN_KEY: 'dnd-client-auth-token',
   ID: 'id'
 }
